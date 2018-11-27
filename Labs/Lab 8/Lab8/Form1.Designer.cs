435337baa60c4d98d6b14b2be1f7e8778c6f35a4
tommy-tran <tvtran315@gmail.com>
2018-11-26 23:47:22 -0500

    Add files via upload

---
+++ Labs/Lab 8/Lab8/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Lab8
+{
+    partial class lab8Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.distancePromptLbl = new System.Windows.Forms.Label();
+            this.distanceTextBox = new System.Windows.Forms.TextBox();
+            this.pricePromptLbl = new System.Windows.Forms.Label();
+            this.priceOutputLbl = new System.Windows.Forms.Label();
+            this.viewPriceBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLbl
+            // 
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLbl.Location = new System.Drawing.Point(46, 9);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(362, 25);
+            this.titleLbl.TabIndex = 0;
+            this.titleLbl.Text = "Whippet Bus Co. Ticket Price Finder";
+            // 
+            // distancePromptLbl
+            // 
+            this.distancePromptLbl.AutoSize = true;
+            this.distancePromptLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.distancePromptLbl.Location = new System.Drawing.Point(142, 69);
+            this.distancePromptLbl.Name = "distancePromptLbl";
+            this.distancePromptLbl.Size = new System.Drawing.Size(170, 22);
+            this.distancePromptLbl.TabIndex = 1;
+            this.distancePromptLbl.Text = "Enter Trip Distance:";
+            // 
+            // distanceTextBox
+            // 
+            this.distanceTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.distanceTextBox.Location = new System.Drawing.Point(170, 103);
+            this.distanceTextBox.Name = "distanceTextBox";
+            this.distanceTextBox.Size = new System.Drawing.Size(100, 28);
+            this.distanceTextBox.TabIndex = 2;
+            this.distanceTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pricePromptLbl
+            // 
+            this.pricePromptLbl.AutoSize = true;
+            this.pricePromptLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pricePromptLbl.Location = new System.Drawing.Point(157, 143);
+            this.pricePromptLbl.Name = "pricePromptLbl";
+            this.pricePromptLbl.Size = new System.Drawing.Size(137, 22);
+            this.pricePromptLbl.TabIndex = 3;
+            this.pricePromptLbl.Text = "Ticket Price ($):";
+            // 
+            // priceOutputLbl
+            // 
+            this.priceOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.priceOutputLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priceOutputLbl.Location = new System.Drawing.Point(170, 177);
+            this.priceOutputLbl.Name = "priceOutputLbl";
+            this.priceOutputLbl.Size = new System.Drawing.Size(100, 28);
+            this.priceOutputLbl.TabIndex = 4;
+            this.priceOutputLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // viewPriceBtn
+            // 
+            this.viewPriceBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewPriceBtn.Location = new System.Drawing.Point(110, 246);
+            this.viewPriceBtn.Name = "viewPriceBtn";
+            this.viewPriceBtn.Size = new System.Drawing.Size(105, 32);
+            this.viewPriceBtn.TabIndex = 5;
+            this.viewPriceBtn.Text = "View Price";
+            this.viewPriceBtn.UseVisualStyleBackColor = true;
+            this.viewPriceBtn.Click += new System.EventHandler(this.viewPriceBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(239, 246);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(105, 32);
+            this.exitBtn.TabIndex = 6;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // lab8Form
+            // 
+            this.AcceptButton = this.viewPriceBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 296);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.viewPriceBtn);
+            this.Controls.Add(this.priceOutputLbl);
+            this.Controls.Add(this.pricePromptLbl);
+            this.Controls.Add(this.distanceTextBox);
+            this.Controls.Add(this.distancePromptLbl);
+            this.Controls.Add(this.titleLbl);
+            this.Name = "lab8Form";
+            this.Text = "Lab 8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.Label distancePromptLbl;
+        private System.Windows.Forms.TextBox distanceTextBox;
+        private System.Windows.Forms.Label pricePromptLbl;
+        private System.Windows.Forms.Label priceOutputLbl;
+        private System.Windows.Forms.Button viewPriceBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ Labs/Lab 8/Lab8/Form1.cs
@@ -0,0 +1,74 @@
+﻿// A1519
+// Lab 8
+// November 13th, 2-16
+// CIS-199-01
+
+// This lab inputs user trip distance and outputs ticket price; performed by range matching parallel arrays of trip distance and ticket price
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8
+{
+    public partial class lab8Form : Form
+    {
+        public lab8Form()
+        {
+            InitializeComponent();
+        }
+
+        private void viewPriceBtn_Click(object sender, EventArgs e)
+        {
+            int[] tripDistanceFloor = { 0, 100, 300, 500 };             // Array that displays the floor distance that matches price range
+            decimal[] ticketPrice = { 25.00m, 40.00m, 55.00m, 70.00m }; // Array that displays the ticket prices
+            bool found = false;        // Boolean to show whether there was a distance match
+            decimal outputPrice = 0m;  // Output price of ticket
+            double tripDistance = 0;      // User-inputted trip distance
+
+            int index = tripDistanceFloor.Length - 1; // Turns position of distances into array index positions
+
+            while (index >= 0 && !found)
+            {
+                if (double.TryParse(distanceTextBox.Text, out tripDistance) && tripDistance >= 0) // Parses user inputs; needs to be greater than 0
+                {
+                    if (tripDistance >= tripDistanceFloor[index]) // Performs array matching
+                    {
+                        found = true;
+                    }
+                    else
+                    {
+                        --index;
+                    }
+
+                    if (found)
+                    {
+                        outputPrice = ticketPrice[index];
+                        priceOutputLbl.Text = outputPrice.ToString();
+                        // If found, output price is the price in the position of the array, and displays it in ouput label.
+                    }
+
+                }
+
+                else
+                {
+                    MessageBox.Show("Please enter a valid trip distance."); // Displays message box if data inputted in invalid
+                }
+
+            }
+
+        }
+
+        // Button that closes form
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
