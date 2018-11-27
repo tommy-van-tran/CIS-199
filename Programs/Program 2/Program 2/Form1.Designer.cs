ee53e03d496261dd3cf6a1dedd75e556dfcf7af1
tommy-tran <tvtran315@gmail.com>
2018-11-26 23:21:38 -0500

    Add files via upload

---
+++ Programs/Program 2/Program 2/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace prog2
+{
+    partial class Prog4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.enterHoursLbl = new System.Windows.Forms.Label();
+            this.enterLastLbl = new System.Windows.Forms.Label();
+            this.resultPromptLbl = new System.Windows.Forms.Label();
+            this.viewResultsBtn = new System.Windows.Forms.Button();
+            this.creditHoursTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.clearFormBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.registrationOutputLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLbl
+            // 
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLbl.Location = new System.Drawing.Point(12, 9);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(581, 29);
+            this.titleLbl.TabIndex = 0;
+            this.titleLbl.Text = "UofL Undergraduate Spring 2017 Priority Registration";
+            // 
+            // enterHoursLbl
+            // 
+            this.enterHoursLbl.AutoSize = true;
+            this.enterHoursLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterHoursLbl.Location = new System.Drawing.Point(12, 92);
+            this.enterHoursLbl.Name = "enterHoursLbl";
+            this.enterHoursLbl.Size = new System.Drawing.Size(444, 22);
+            this.enterHoursLbl.TabIndex = 0;
+            this.enterHoursLbl.Text = "Enter your completed credit hours PRIOR to Fall 2016:";
+            // 
+            // enterLastLbl
+            // 
+            this.enterLastLbl.AutoSize = true;
+            this.enterLastLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterLastLbl.Location = new System.Drawing.Point(248, 135);
+            this.enterLastLbl.Name = "enterLastLbl";
+            this.enterLastLbl.Size = new System.Drawing.Size(208, 22);
+            this.enterLastLbl.TabIndex = 0;
+            this.enterLastLbl.Text = "Enter your full last name:";
+            // 
+            // resultPromptLbl
+            // 
+            this.resultPromptLbl.AutoSize = true;
+            this.resultPromptLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultPromptLbl.Location = new System.Drawing.Point(132, 239);
+            this.resultPromptLbl.Name = "resultPromptLbl";
+            this.resultPromptLbl.Size = new System.Drawing.Size(341, 22);
+            this.resultPromptLbl.TabIndex = 0;
+            this.resultPromptLbl.Text = "Your earliest registration time and date is:";
+            // 
+            // viewResultsBtn
+            // 
+            this.viewResultsBtn.Location = new System.Drawing.Point(12, 382);
+            this.viewResultsBtn.Name = "viewResultsBtn";
+            this.viewResultsBtn.Size = new System.Drawing.Size(181, 40);
+            this.viewResultsBtn.TabIndex = 3;
+            this.viewResultsBtn.Text = "View Results";
+            this.viewResultsBtn.UseVisualStyleBackColor = true;
+            this.viewResultsBtn.Click += new System.EventHandler(this.viewResultsBtn_Click);
+            // 
+            // creditHoursTextBox
+            // 
+            this.creditHoursTextBox.Location = new System.Drawing.Point(462, 88);
+            this.creditHoursTextBox.Name = "creditHoursTextBox";
+            this.creditHoursTextBox.Size = new System.Drawing.Size(125, 26);
+            this.creditHoursTextBox.TabIndex = 1;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(462, 131);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(125, 26);
+            this.lastNameTextBox.TabIndex = 2;
+            // 
+            // clearFormBtn
+            // 
+            this.clearFormBtn.Location = new System.Drawing.Point(211, 382);
+            this.clearFormBtn.Name = "clearFormBtn";
+            this.clearFormBtn.Size = new System.Drawing.Size(181, 40);
+            this.clearFormBtn.TabIndex = 4;
+            this.clearFormBtn.Text = "Clear Form";
+            this.clearFormBtn.UseVisualStyleBackColor = true;
+            this.clearFormBtn.Click += new System.EventHandler(this.clearFormBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(410, 382);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(181, 40);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // registrationOutputLbl
+            // 
+            this.registrationOutputLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registrationOutputLbl.Location = new System.Drawing.Point(95, 272);
+            this.registrationOutputLbl.Name = "registrationOutputLbl";
+            this.registrationOutputLbl.Size = new System.Drawing.Size(415, 23);
+            this.registrationOutputLbl.TabIndex = 0;
+            this.registrationOutputLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Prog4
+            // 
+            this.AcceptButton = this.viewResultsBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 444);
+            this.Controls.Add(this.registrationOutputLbl);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearFormBtn);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.creditHoursTextBox);
+            this.Controls.Add(this.viewResultsBtn);
+            this.Controls.Add(this.resultPromptLbl);
+            this.Controls.Add(this.enterLastLbl);
+            this.Controls.Add(this.enterHoursLbl);
+            this.Controls.Add(this.titleLbl);
+            this.Name = "Prog4";
+            this.Text = "Program 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.Label enterHoursLbl;
+        private System.Windows.Forms.Label enterLastLbl;
+        private System.Windows.Forms.Label resultPromptLbl;
+        private System.Windows.Forms.Button viewResultsBtn;
+        private System.Windows.Forms.TextBox creditHoursTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Button clearFormBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label registrationOutputLbl;
+    }
+}
+
---
+++ Programs/Program 2/Program 2/Form1.cs
@@ -0,0 +1,236 @@
+﻿// A1519
+// Program 2
+// October 16, 2016
+// CIS 199-01
+
+// This program inputs credit hours and a last name, and outputs the date and time of the earliest registration
+// time (using UofL Spring 2017 registration times).
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prog2
+{
+    public partial class Prog4 : Form
+    {
+        public Prog4()
+        {
+            InitializeComponent();
+        }
+        
+        private void viewResultsBtn_Click(object sender, EventArgs e)
+        {
+            bool FRESHMAN = true, SOPHOMORE = true, JUNIOR = true, SENIOR = true; // Assigns boolean values to test for each class of students
+            float CREDIT_HOURS; // Assigns credit hours
+            string LAST_NAME = lastNameTextBox.Text; // Assigns a last name string 
+            string LAST_NAME_LOW = LAST_NAME.ToLower(); // Assigns a last name with all lowercase for easier testing
+            char LAST_NAME_CHAR = LAST_NAME_LOW[0]; // Assigns char from first character of the last name
+
+            // Tests to see if inputs are valid
+            if ((float.TryParse(creditHoursTextBox.Text, out CREDIT_HOURS) && CREDIT_HOURS >= 0) && char.IsLetter(lastNameTextBox.Text[0]))
+            {
+
+                // Assigns the class standing (Senior, Junior, Sophomore or Freshman) based on inputted credit hours
+                if (CREDIT_HOURS >= 90) 
+                {
+                    SENIOR = true;
+                    JUNIOR = false;
+                    SOPHOMORE = false;
+                    FRESHMAN = false;
+                }
+                else if (CREDIT_HOURS < 90 && CREDIT_HOURS >= 60)
+                {
+                    JUNIOR = true;
+                    SENIOR = false;
+                    SOPHOMORE = false;
+                    FRESHMAN = false;
+                }
+                else if (CREDIT_HOURS < 60 && CREDIT_HOURS >= 30)
+                {
+                    SOPHOMORE = true;
+                    SENIOR = false;
+                    JUNIOR = false;
+                    FRESHMAN = false;
+                }
+                else if (CREDIT_HOURS < 30 && CREDIT_HOURS >=0)
+                {
+                    FRESHMAN = true;
+                    SENIOR = false;
+                    JUNIOR = false;
+                    SOPHOMORE = false;
+                }
+
+                // Based on class standing and first char of last name, outputs the earliest registration time 
+                if (SENIOR)
+                {
+                    if (LAST_NAME_CHAR >= 'j' && LAST_NAME_CHAR <= 'o')
+                    {
+                        registrationOutputLbl.Text = "Friday, November 4, 8:30 AM";
+                    }
+                    else if (LAST_NAME_CHAR >= 'p' && LAST_NAME_CHAR <= 's')
+                    {
+                        registrationOutputLbl.Text = "Friday, November 4, 10:00 AM";
+                    }
+                    else if (LAST_NAME_CHAR >= 't' && LAST_NAME_CHAR <= 'z')
+                    {
+                        registrationOutputLbl.Text = "Friday, November 4, 11:30 AM";
+                    }
+                    else if (LAST_NAME_CHAR >= 'a' && LAST_NAME_CHAR <= 'd')
+                    {
+                        registrationOutputLbl.Text = "Friday, November 4, 2:00 PM";
+                    }
+                    else if (LAST_NAME_CHAR >= 'e' && LAST_NAME_CHAR <= 'i')
+                    {
+                        registrationOutputLbl.Text = "Friday, November 4, 4:00 PM";
+                    }
+                }
+
+
+                else if (JUNIOR)
+                {
+                    if (LAST_NAME_CHAR >= 'j' && LAST_NAME_CHAR <= 'o')
+                    {
+                        registrationOutputLbl.Text = "Monday, November 7, 8:30 AM";
+                    }
+                    else if (LAST_NAME_CHAR >= 'p' && LAST_NAME_CHAR <= 's')
+                    {
+                        registrationOutputLbl.Text = "Monday, November 7, 10:00 AM";
+                    }
+                    else if (LAST_NAME_CHAR >= 't' && LAST_NAME_CHAR <= 'z')
+                    {
+                        registrationOutputLbl.Text = "Monday, November 7, 11:30 AM";
+                    }
+                    else if (LAST_NAME_CHAR >= 'a' && LAST_NAME_CHAR <= 'd')
+                    {
+                        registrationOutputLbl.Text = "Monday, November 7, 2:00 PM";
+                    }
+                    else if (LAST_NAME_CHAR >= 'e' && LAST_NAME_CHAR <= 'i')
+                    {
+                        registrationOutputLbl.Text = "Monday, November 7, 4:00 PM";
+                    }
+                }
+
+                    if (SOPHOMORE)
+                    {
+                        if (LAST_NAME_CHAR >= 'j' && LAST_NAME_CHAR <= 'l')
+                        {
+                            registrationOutputLbl.Text = "Wednesday, November 9, 8:30 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'm' && LAST_NAME_CHAR <= 'o')
+                        {
+                            registrationOutputLbl.Text = "Wednesday, November 9,, 10:00 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'p' && LAST_NAME_CHAR <= 'q')
+                        {
+                            registrationOutputLbl.Text = "Wednesday, November 9, 11:30 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'r' && LAST_NAME_CHAR <= 's')
+                        {
+                            registrationOutputLbl.Text = "Wednesday, November 9, 2:00 PM";
+                        }
+                        else if (LAST_NAME_CHAR >= 't' && LAST_NAME_CHAR <= 'v')
+                        {
+                            registrationOutputLbl.Text = "Wednesday, November 9, 4:00 PM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'w' && LAST_NAME_CHAR <= 'z')
+                        {
+                            registrationOutputLbl.Text = "Thursday, November 10, 8:30 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'a' && LAST_NAME_CHAR <= 'b')
+                        {
+                            registrationOutputLbl.Text = "Thursday, November 10, 10:00 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'c' && LAST_NAME_CHAR <= 'd')
+                        {
+                            registrationOutputLbl.Text = "Thursday, November 10, 11:30 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'e' && LAST_NAME_CHAR <= 'f')
+                        {
+                            registrationOutputLbl.Text = "Thursday, November 10, 2:00 PM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'g' && LAST_NAME_CHAR <= 'h')
+                        {
+                            registrationOutputLbl.Text = "Thursday, November 10, 4:00 PM";
+                        }
+                    }
+
+                    if (FRESHMAN)
+                    {
+                        if (LAST_NAME_CHAR >= 'j' && LAST_NAME_CHAR <= 'l')
+                        {
+                            registrationOutputLbl.Text = "Friday, November 11, 8:30 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'm' && LAST_NAME_CHAR <= 'o')
+                        {
+                            registrationOutputLbl.Text = "Friday, November 11, 10:00 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'p' && LAST_NAME_CHAR <= 'q')
+                        {
+                            registrationOutputLbl.Text = "Friday, November 11, 11:30 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'r' && LAST_NAME_CHAR <= 's')
+                        {
+                            registrationOutputLbl.Text = "Friday, November 11, 2:00 PM";
+                        }
+                        else if (LAST_NAME_CHAR >= 't' && LAST_NAME_CHAR <= 'v')
+                        {
+                            registrationOutputLbl.Text = "Friday, November 11, 4:00 PM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'w' && LAST_NAME_CHAR <= 'z')
+                        {
+                            registrationOutputLbl.Text = "Monday, November 14, 8:30 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'a' && LAST_NAME_CHAR <= 'b')
+                        {
+                            registrationOutputLbl.Text = "Monday, November 14, 10:00 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'c' && LAST_NAME_CHAR <= 'd')
+                        {
+                            registrationOutputLbl.Text = "Monday, November 14, 11:30 AM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'e' && LAST_NAME_CHAR <= 'f')
+                        {
+                            registrationOutputLbl.Text = "Monday, November 14, 2:00 PM";
+                        }
+                        else if (LAST_NAME_CHAR >= 'g' && LAST_NAME_CHAR <= 'h')
+                        {
+                            registrationOutputLbl.Text = "Monday, November 14, 4:00 PM";
+                        }
+                    }
+                }
+            
+            // Tests to see if fields are left blank (doesn't work for some reason)
+            else if (string.IsNullOrWhiteSpace(creditHoursTextBox.Text) || char.IsWhiteSpace(LAST_NAME_CHAR))
+            {
+                MessageBox.Show("Please enter a non-zero number for credit hours and a valid last name.");
+            }
+
+            // If inputs are invalid, show message box
+            else
+            {
+                MessageBox.Show("Please enter a non-zero number for credit hours and a valid last name.");
+            }
+        }
+
+        // Button that clears form
+        private void clearFormBtn_Click(object sender, EventArgs e)
+        {
+            creditHoursTextBox.Clear();
+            lastNameTextBox.Clear();
+            registrationOutputLbl.Text = "";
+        }
+
+        // Button that closes form
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
