0a5851dc86109e23ded8dcf2ce8bb19f81c7e345
tommy-tran <tvtran315@gmail.com>
2018-11-26 23:23:30 -0500

    Add files via upload

---
+++ Programs/Program 3/Program 3/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Program3
+{
+    partial class prog3Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimeLbl = new System.Windows.Forms.Label();
+            this.RegLbl = new System.Windows.Forms.Label();
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.creditHrLbl = new System.Windows.Forms.Label();
+            this.findRegTimeBtn = new System.Windows.Forms.Button();
+            this.lastNameTxt = new System.Windows.Forms.TextBox();
+            this.creditHrTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dateTimeLbl
+            // 
+            this.dateTimeLbl.AutoSize = true;
+            this.dateTimeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimeLbl.Location = new System.Drawing.Point(102, 266);
+            this.dateTimeLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.dateTimeLbl.Name = "dateTimeLbl";
+            this.dateTimeLbl.Size = new System.Drawing.Size(0, 20);
+            this.dateTimeLbl.TabIndex = 13;
+            // 
+            // RegLbl
+            // 
+            this.RegLbl.AutoSize = true;
+            this.RegLbl.Location = new System.Drawing.Point(98, 240);
+            this.RegLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.RegLbl.Name = "RegLbl";
+            this.RegLbl.Size = new System.Drawing.Size(229, 20);
+            this.RegLbl.TabIndex = 12;
+            this.RegLbl.Text = "The earliest you may register is:";
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Location = new System.Drawing.Point(160, 97);
+            this.nameLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(88, 20);
+            this.nameLbl.TabIndex = 11;
+            this.nameLbl.Text = "Last name:";
+            // 
+            // creditHrLbl
+            // 
+            this.creditHrLbl.AutoSize = true;
+            this.creditHrLbl.Location = new System.Drawing.Point(44, 56);
+            this.creditHrLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.creditHrLbl.Name = "creditHrLbl";
+            this.creditHrLbl.Size = new System.Drawing.Size(185, 20);
+            this.creditHrLbl.TabIndex = 10;
+            this.creditHrLbl.Text = "Credits prior to Fall 2016:";
+            // 
+            // findRegTimeBtn
+            // 
+            this.findRegTimeBtn.Location = new System.Drawing.Point(138, 165);
+            this.findRegTimeBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.findRegTimeBtn.Name = "findRegTimeBtn";
+            this.findRegTimeBtn.Size = new System.Drawing.Size(148, 35);
+            this.findRegTimeBtn.TabIndex = 9;
+            this.findRegTimeBtn.Text = "Find Reg. Time";
+            this.findRegTimeBtn.UseVisualStyleBackColor = true;
+            this.findRegTimeBtn.Click += new System.EventHandler(this.findRegTimeBtn_Click);
+            // 
+            // lastNameTxt
+            // 
+            this.lastNameTxt.Location = new System.Drawing.Point(258, 93);
+            this.lastNameTxt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lastNameTxt.Name = "lastNameTxt";
+            this.lastNameTxt.Size = new System.Drawing.Size(148, 26);
+            this.lastNameTxt.TabIndex = 8;
+            // 
+            // creditHrTxt
+            // 
+            this.creditHrTxt.Location = new System.Drawing.Point(258, 51);
+            this.creditHrTxt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.creditHrTxt.Name = "creditHrTxt";
+            this.creditHrTxt.Size = new System.Drawing.Size(148, 26);
+            this.creditHrTxt.TabIndex = 7;
+            // 
+            // prog3Form
+            // 
+            this.AcceptButton = this.findRegTimeBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 337);
+            this.Controls.Add(this.dateTimeLbl);
+            this.Controls.Add(this.RegLbl);
+            this.Controls.Add(this.nameLbl);
+            this.Controls.Add(this.creditHrLbl);
+            this.Controls.Add(this.findRegTimeBtn);
+            this.Controls.Add(this.lastNameTxt);
+            this.Controls.Add(this.creditHrTxt);
+            this.Name = "prog3Form";
+            this.Text = "Program 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label dateTimeLbl;
+        private System.Windows.Forms.Label RegLbl;
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label creditHrLbl;
+        private System.Windows.Forms.Button findRegTimeBtn;
+        private System.Windows.Forms.TextBox lastNameTxt;
+        private System.Windows.Forms.TextBox creditHrTxt;
+    }
+}
+
---
+++ Programs/Program 3/Program 3/Form1.cs
@@ -0,0 +1,162 @@
+﻿// A1519
+// CIS-199-01
+// Program 3
+// 11/20/2016
+
+// Using range matching and parallel arrays, takes user input of credit hours and last name and outputs registration time for Spring 2017 at UofL. 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program3
+{
+    public partial class prog3Form : Form
+    {
+        public prog3Form()
+        {
+            InitializeComponent();
+        }
+
+        // Precondition: Input of valid credit hours (>0) and last name (string); click button
+        // Postcondition: Output of registration time
+        private void findRegTimeBtn_Click(object sender, EventArgs e)
+        {
+            const float SENIOR_HOURS = 90;    // Min hours for Senior
+            const float JUNIOR_HOURS = 60;    // Min hours for Junior
+            const float SOPHOMORE_HOURS = 30; // Min hours for Soph.
+
+            // Array of Senior Registration Dates listed in alphabetic order
+            string[] seniorRegistrationSchedule = {"November 4th, 2:00 PM", "November 4th, 4:00 PM", "November 4th, 8:30 AM",
+                                                   "November 4th, 10:00 AM", "November 4th, 11:30 AM"};
+
+            // Array of Junior Registration Dates listed in alphabetic order
+            string[] juniorRegistrationSchedule = {"November 7th, 2:00 PM", "November 7th, 4:00 PM", "November 7th, 8:30 AM",
+                                                   "November 7th, 10:00 AM", "November 7th, 11:30 AM"};
+            
+            // Array of Sophomore Registration Dates listed in alphabetic order
+            string[] sophomoreRegistrationSchedule = {"November 10th, 10:00 AM", "November 10th, 11:30 AM", "November 10th, 2:00 PM", "November 10th, 4:00 PM",
+                                                      "November 9th, 8:30 AM", "November 9th, 10:00 AM", "November 9th, 11:30 AM", "November 9th, 2:00 PM",
+                                                      "November 9th, 4:00 PM", "November 10th, 8:30 AM"};
+
+            // Array of Freshmen Registration Dates listed in alphabetic order
+            string[] freshmenRegistrationSchedule = {"November 14th, 10:00 AM", "November 14th, 11:30 AM", "November 14th, 2:00 PM", "November 14th, 4:00 PM",
+                                                     "November 11th, 8:30 AM", "November 11th, 10:00 AM", "November 11th, 11:30 AM", "November 11th, 2:00 PM",
+                                                     "November 11th, 4:00 PM", "November 14th, 8:30 AM"};
+
+            // Array of upperclassmen last name chars matched to registration dates
+            char[] upperClassLastNameOrder = { 'D', 'I', 'O', 'S', 'Z' };
+
+            // Array of underclassmen last name chars matched to registration dates
+            char[] underClassLastNameOrder = { 'B', 'D', 'F', 'I', 'L', 'O', 'Q', 'S', 'V', 'Z' };
+
+            string lastNameStr;       // Entered last name
+            char lastNameLetterCh;    // First letter of last name, as char
+            float creditHours;        // Entered credit hours
+            bool found = false;       // Boolean for parallel array / range matching
+
+            if (float.TryParse(creditHrTxt.Text, out creditHours) && creditHours >= 0) // Valid hours
+            {
+                lastNameStr = lastNameTxt.Text;
+                if (lastNameStr.Length > 0) // Empty string?
+                {
+                    lastNameLetterCh = lastNameStr[0];   // First char of last name
+                    lastNameLetterCh = char.ToUpper(lastNameLetterCh); // Ensure upper case
+
+                    if (char.IsLetter(lastNameLetterCh)) // Is it a letter?
+                    {
+                        // Juniors and Seniors share same schedule but different days
+                        int index = 0;
+                        if (creditHours >= JUNIOR_HOURS)
+                        {
+                            if (creditHours >= SENIOR_HOURS)
+                            {
+                                while (index <= upperClassLastNameOrder.Length - 1 && !found)
+                                {
+                                    if (lastNameLetterCh <= upperClassLastNameOrder[index])
+                                        found = true;
+                                    else
+                                        ++index;
+                                }
+
+                                if (found)
+                                {
+                                    RegLbl.Text = seniorRegistrationSchedule[index];
+                                }
+                            }
+
+                            else // Must be juniors
+                            {
+                                index = 0;
+                                while (index <= upperClassLastNameOrder.Length - 1 && !found)
+                                {
+                                    if (lastNameLetterCh <= upperClassLastNameOrder[index])
+                                        found = true;
+                                    else
+                                        ++index;
+                                }
+
+                                if (found)
+                                {
+                                    RegLbl.Text = juniorRegistrationSchedule[index];
+                                }
+                            }
+
+                        }
+                        // Sophomores and Freshmen
+                        else // Must be soph/fresh
+                        {
+                            if (creditHours >= SOPHOMORE_HOURS)
+                            {
+                                index = 0;
+                                while (index <= underClassLastNameOrder.Length - 1 && !found)
+                                {
+                                    if (lastNameLetterCh <= underClassLastNameOrder[index])
+                                        found = true;
+                                    else
+                                        ++index;
+                                }
+
+                                if (found)
+                                {
+                                    RegLbl.Text = sophomoreRegistrationSchedule[index];
+                                }
+
+                            }
+                            else // must be freshman
+                            {
+                                index = 0;
+                                while (index <= underClassLastNameOrder.Length - 1 && !found)
+                                {
+                                    if (lastNameLetterCh <= underClassLastNameOrder[index])
+                                        found = true;
+                                    else
+                                        ++index;
+                                }
+
+                                if (found)
+                                {
+                                    RegLbl.Text = freshmenRegistrationSchedule[index];
+                                }
+                            }
+
+
+                        }
+                    }
+                    else // First char not a letter
+                        MessageBox.Show("Enter valid last name!");
+                }
+                else // Empty textbox
+                    MessageBox.Show("Enter a last name!");
+            }
+            else // Can't parse credit hours
+                MessageBox.Show("Please enter valid credit hours earned!");
+        }
+    }
+}
